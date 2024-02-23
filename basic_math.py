26fccf0a3b397f598aa640ee15dc255070a20de5
Jack Halverson <jhalverson4838@xmail.dixietech.edu>
2024-02-23 08:52:33 -0700

    Create basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,22 @@
+def add(x: float, y: float) -> float:
+    return(float(x)+float(y))
+
+def multiply(x: float, y: float) -> float:
+    return(float(x)*float(y))
+
+def square(x: float) -> float:
+    return(multiply(x, x))
+
+def add_squares(x: float, y: float) -> float:
+    x = square(x)
+    y = square(y)
+    return(multiply(x,y))
+
+def main():
+    a=add(3,4)
+    b=multiply(3,4)
+    c=square(5)
+    d=add_squares(3,4)
+    print(a,b,c,d)
+
+main()
